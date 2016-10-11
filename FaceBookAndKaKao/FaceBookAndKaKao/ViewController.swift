3c2b4c30b8cd5d3f29544bac80d9bfd4a7d4f5e6
luffy1022 <luffy1021@naver.com>
2016-10-11 10:41:12 +0900

    FirstCommit

---
+++ FaceBookAndKaKao/FaceBookAndKaKao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceBookAndKaKao
+//
+//  Created by Jason on 2016. 10. 11..
+//  Copyright © 2016년 JasonYoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
